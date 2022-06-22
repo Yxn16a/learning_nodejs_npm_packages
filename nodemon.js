f1ce458e1cc2dfa315b3bb0c73c678b6920278c2
Yves <yxn16a@acu.edu>
2022-06-22 10:12:23 +0200

    Learned nodemon, jsonpackage file content and json-lock-file

---
+++ nodemon.js
@@ -0,0 +1,12 @@
+// this is used nodemon available 
+// nodemon used to run code for any change that we make in those code
+// nodemon is excutable
+// and the way to run nodemon is to use the scrip in package.jsos file
+// nodemon is not recognized in the terminal and it is only living in the depencies
+// whe we want to run nodemon
+// we should run the following common in the command line 
+// ./node_modules/.bin/nodemon request.jst  this is not common to run the application
+// to use nodemon at any computer not even downloading it 
+// we do this to  be able to to run 
+// nodemon request.js can be run 
+// avoid to install package grobally
