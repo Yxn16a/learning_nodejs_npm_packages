fccc1c410110126a5f6a358eaa98a6f72f82d124
Yves <yxn16a@acu.edu>
2022-06-23 09:26:52 +0200

    compared different frameworks

---
+++ expressframework/frameworkComparison.js
@@ -0,0 +1,13 @@
+// high level view of express
+// it was created in 2010 
+// it used callback functions 
+// express is good at routing 
+// it is super realiable 
+// middle ware allows how to manipulate responses 
+// LoopBack 4: it is difficult to learn 
+// Koa: designed to be promise based instead of using call back function
+// koa bandles resp and request, 
+// next js: based on the implementing react in the back end 
+// next js: helps in landering at the server side
+// next js: support type scripts, it focuses  mostly on the react 
+// all these framwork have commonality with express that we think
