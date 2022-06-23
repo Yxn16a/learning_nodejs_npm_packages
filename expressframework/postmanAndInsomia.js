3876b460a4f2630340cf5140490c4e365b6b9df3
Yves <yxn16a@acu.edu>
2022-06-23 10:03:06 +0200

    looked at postman and insomia

---
+++ expressframework/postmanAndInsomia.js
@@ -0,0 +1,9 @@
+// Postman 
+// the best way test api 
+// provide automated testing 
+// design and mock
+// post man will document your api to share with other developers
+//  it helps in monitoring your work
+// it provide workspaces 
+// insomia: it is easy to use api testing tools which does the same as
+// post man
