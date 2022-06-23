ef53fe9c247a51144ad48610bd221c38a420e314
Yves <yxn16a@acu.edu>
2022-06-23 17:05:48 +0200

    rest introduction

---
+++ resfulApi/restIntro.js
@@ -0,0 +1,20 @@
+// endpoints: are called with prural name and they are called collections
+// rest:representational state transfer
+// state and representational: how server make the data available 
+// transfer: how data is transfered to the user
+//rest must use already existing standards
+// those include HTTP, JSON(java script standard data), URLS 
+// use what you already have in web 
+// endpoints represent the collection of data
+// Get, post, put, Delete : are used to do action in the server 
+// client and server
+// request are statatless and cacheable
+// request are not connected and they are statless which means they 
+// are independent of the client and they are cacheable which means we 
+// save the request the client makes and next time they request it 
+// we do not search for it but we do not keep track of the user who are requesting 
+// the data as in php
+// in rest we do not need to track the number of users in sessions  as php does
+// this minimizes the work  that the server does 
+// back them:
+// functions were being used 
