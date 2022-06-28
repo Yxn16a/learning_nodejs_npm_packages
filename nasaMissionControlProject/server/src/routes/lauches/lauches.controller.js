bef922137c0d63ad54edc882f673035c69457d16
yves <yxn16a@acu.edu>
2022-06-28 15:31:21 +0200

    added validation before the persing of the data

--- nasaMissionControlProject/server/src/routes/lauches/lauches.controller.js
+++ nasaMissionControlProject/server/src/routes/lauches/lauches.controller.js
@@ -1,19 +1,36 @@
-
 // This is business logic 
-const { getAllLaunches,
-        AddNewLaunch} = require('../../models/launches.model')
-function httpGetAllLaunches(req, res) { 
+const {
+    getAllLaunches,
+    AddNewLaunch
+} = require('../../models/launches.model')
+
+function httpGetAllLaunches(req, res) {
     // this returns the array of the collection of map objects
-   // return res.status(200).json(Array.from(launches.values())); taken to models
+    // return res.status(200).json(Array.from(launches.values())); taken to models
     return res.status(200).json(getAllLaunches())
 };
 
-function httpAddNewLaunch(req, res) { 
+function httpAddNewLaunch(req, res) {
     const launch = req.body;
     // this will convert th 
+    if (!launch.mission || !launch.rocket || !launch.launchDate ||
+        !launch.destination) {
+        // 404 not fund
+        return res.status(400).json({
+            erro: "Missing required launch property",
+        });
+    }
     launch.launchDate = new Date(launch.launchDate)
-    AddNewLaunch(launch); 
-    return res.status(201).json(launch); 
+    // validate the date by converting it toString
+    // this is done when the date can not be converted toString
+    // this is not a number checks i
+    if (isNaN(launch.launchDate)) {
+        return res.status(400).json({
+            error: "Invalid launch date"
+        })
+    }
+    AddNewLaunch(launch);
+    return res.status(201).json(launch);
 }
 module.exports = {
     httpGetAllLaunches,
